edcede62ca6fcb1eea984752323260e4fa912446
jesse <jes1510@gmail.com>
2017-05-11 23:23:45 -0400

    Integrated LCD Changed to STM32F103 Nucleo board pulled out software serial for now

--- code/nerfbot/nerfbot.ino
+++ code/nerfbot/nerfbot.ino
@@ -1,18 +1,33 @@
 
 // Requires serial command library:  https://github.com/kroimon/Arduino-SerialCommand
 
-#include <SoftwareSerial.h>
-#include <PololuQik.h>
+//#include <SoftwareSerial.h>
+//#include <PololuQik.h>
 #include <SerialCommand.h>
 
-int versionMajor = 0;
-int versionMinor = 1;
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+//
+//
+//
+//// If using software SPI (the default case):
+#define OLED_MOSI   11
+#define OLED_CLK   13
+#define OLED_DC    12
+#define OLED_CS    10
+#define OLED_RESET 18
+
+Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 1
 
 int motorErrorPin = 15;
 
 SerialCommand sCmd;     // The demo SerialCommand object
 
-PololuQik2s12v10 qik(10, 11, 14);
+//PololuQik2s12v10 qik(16, 17, 14);
 
 int channelPins[8];
 unsigned long pulses[8];
@@ -23,29 +38,20 @@ bool sentFlag = false;
 bool sentPrompt = false;
 bool streamPulses = false;
 
-//
-//struct Command {
-//  char cmd;
-//  void funct;  
-//};
-//
-//void cmdVer(void);
-//
-//struct Commands {
-//  Command c1{"ver", cmdVer};
-//};
-//
-//Commands commands;
 void setup() {
-
   int i;
+  int pin;
+  
   for (i=0; i<8; i++){
-    channelPins[i] = i+2;  // Pulses are on Pins D2-D9
+    pin = i + 2;
+    channelPins[i] = pin;  // Pulses are on Pins D2-D9
+    pinMode(pin, INPUT);
     pulses[i] = 0;  
   }
 
+  
+
   sCmd.addCommand("ver", cmdVer);
-//  sCmd.addCommand(commands.c1.cmd, commands.c1.funct);
   sCmd.addCommand("pulses", cmdShowPulses);
   sCmd.addCommand("stream", cmdStream);
   sCmd.addCommand("stopstream", cmdStopStream);
@@ -54,9 +60,8 @@ void setup() {
   
   sCmd.setDefaultHandler(unrecognized);     
 
-  pinMode(15, INPUT);
-  
-  
+  pinMode(15, INPUT);  
+    
   Serial.begin(9600);
   Serial.println("NerfBot");
   Serial.println("qik 2s12v10 dual serial motor controller");
@@ -64,12 +69,21 @@ void setup() {
  // Serial.print("Firmware version: ");
  // Serial.println(qik.getFirmwareVersion());
   Serial.println();
-//  Serial.print(commands.c1.cmd);
+
+  display.begin(SSD1306_SWITCHCAPVCC);
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // Clear the buffer.
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   readPulses();
+    
+ // if (pulses[0] == 0) Serial.println("ERROR!");
+  
 
   if(!sentPrompt) {
     sendPrompt();
@@ -81,23 +95,83 @@ void loop() {
 
   if (streamPulses) printPulses();
 
-  qik.setM0Speed(remap(pulses[0]));
-  qik.setM1Speed(remap(pulses[1]));
-  if(digitalRead(motorErrorPin)==HIGH) {   
-      Serial.println("\n\tMOTOR ERROR!");
-      cmdResetMotor();
-      sendPrompt();
-      delay(250);    
-  }
+  displayPulses();
+
+//  qik.setM0Speed(remap(pulses[0]));
+//  qik.setM1Speed(remap(pulses[1]));
+//  if(digitalRead(motorErrorPin)==HIGH) {   
+//      Serial.println("\n\tMOTOR ERROR!");
+//      cmdResetMotor();
+//      sendPrompt();
+//      delay(250);    
+//  }
   
 
 }
 
-void readPulses() {
+int pulseToBar(unsigned long pulse) {
+  pulse = constrain(pulse, 980, 1900);
+  return map(pulse, 980, 1900, 0, 30);  
+}
+
+void displayPulses() {
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setCursor(15,0);
+  display.println("Receiver");
+  display.drawLine(0, 35, 127, 35, WHITE);
+
   int i;
-  for (i=0; i<8; i++){
-    pulses[i] = pulseIn(channelPins[i], HIGH, 25000);
-  }  
+  int bar;
+  uint8_t color = WHITE;
+  display.setTextSize(1);
+  display.setTextColor(WHITE);  
+
+  for(i=0; i<8; i++){
+    display.setTextSize(1);
+
+    display.setCursor(i * 16 + 4, 56);
+    display.write(i + 1 + '0');  
+    
+    bar = pulseToBar(pulses[i]);
+    
+    if (bar>15) display.fillRect(i * 16 + 4, 35, 6, bar-15, color%2);
+    else {
+      int topBar, loc;
+      topBar = map(bar, 0, 15, 15, 0);
+      if (i==0) Serial.println(topBar);
+      loc = 35-(topBar);
+      display.fillRect(i * 16 + 4, 35-topBar, 6, topBar , color%2);
+    }
+ 
+  }
+    display.display();
+}
+
+unsigned long fixedPulsein(int pin, int timeout) {
+  int start = millis();
+  unsigned long pulse = 0;
+  while (pulse == 0) {
+    pulse = pulseIn(pin, HIGH);
+
+    if (millis() - start > timeout) {
+ 
+      break;
+    }
+  }
+  return pulse;
+}
+
+void readPulses() {
+  int i = 0;
+  unsigned long pulse =0;
+  int start = millis();
+  pulse = fixedPulsein(channelPins[i], 50);
+
+ // Serial.print(millis() - start);
+  pulses[i] = pulse;
+ // Serial.print("\t");
+//  Serial.println(pulse); 
 }
 
 void printPulses() {
@@ -111,7 +185,7 @@ void printPulses() {
 void sendPrompt() {
   if(!quiet) {
     char line[16];
-    sprintf(line, "Nerfbot %i.%i> ", versionMajor, versionMinor);
+    sprintf(line, "Nerfbot %i.%i> ", VERSION_MAJOR, VERSION_MINOR);
     Serial.println(line);
     sentPrompt = true;
   }
@@ -119,7 +193,7 @@ void sendPrompt() {
 
 void cmdVer() {  
   char line[16];
-  sprintf(line, "\tVersion: %i.%i", versionMajor, versionMinor);
+  sprintf(line, "\tVersion: %i.%i", VERSION_MAJOR, VERSION_MINOR);
   Serial.println(line);
   sendPrompt();
 }
@@ -143,7 +217,7 @@ void cmdStream() {
 
   number = atoi(arg);
 
-  if (number) {
+  if (number == 1) {
     streamPulses = true; 
   }
 
@@ -172,7 +246,7 @@ void cmdResetMotor () {
   delay(500);
   digitalWrite(14, HIGH);
   delay(250);
-  qik.init();
+//  qik.init();
   sendPrompt();
 }
 
@@ -183,7 +257,7 @@ bool getMotorError() {
 void cmdGetMotorError() {
  // bool state;
  Serial.print("\tError Byte:");
- Serial.println(qik.getErrors(), HEX);
+// Serial.println(qik.getErrors(), HEX);
  sendPrompt();
 }
 
